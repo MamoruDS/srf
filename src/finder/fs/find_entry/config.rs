c8beb5dc9184cbf39f4ee79b0fd60a4efbe5b37e
MamoruDS <mamoruds.io@gmail.com>
2023-08-30 15:33:05 +0900

    add: config for `FindEntry`

---
+++ src/finder/fs/find_entry/config.rs
@@ -0,0 +1,29 @@
+use serde::Deserialize;
+use std::collections::HashMap;
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct WalkdirOptions {
+    pub follow_links: Option<bool>,
+    pub max_depth: Option<usize>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct Entry {
+    pub root: String,
+    pub options: Option<WalkdirOptions>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(untagged)]
+pub enum EntryConfig {
+    Root(String),
+    Configured(Entry),
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct FindEntryConfig {
+    pub roots: Vec<EntryConfig>,
+    pub pattern: String,
+    pub template: String,
+    pub renames: Option<HashMap<String, Vec<(String, String)>>>,
+}
