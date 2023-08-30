a4a72ba9405a2a154e53a2f8a4c83001890a72fc
MamoruDS <mamoruds.io@gmail.com>
2023-08-30 15:57:39 +0900

    update:

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,35 @@
+use clap::Parser;
+
+mod cli;
+mod finder;
 mod server;
-fn main() {}
+
+use cli::{Args, Commands};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let args = Args::parse();
+    let routes = finder::get_finder_from_yaml(&args.config);
+
+    match &args.command {
+        Commands::Server(_server_args) => {
+            unimplemented!()
+        }
+        Commands::Cli(cli_args) => {
+            if !cli_args.interactive {
+                let entries = routes.get(&cli_args.route).unwrap();
+                for input in cli_args.inputs.iter() {
+                    for entry in entries.iter() {
+                        let results = entry.find(input);
+                        if !results.is_empty() {
+                            println!("{:?}", results);
+                        }
+                    }
+                }
+            } else {
+                println!("Interactive mode is not implemented yet.");
+            }
+            Ok(())
+        }
+    }
+}
